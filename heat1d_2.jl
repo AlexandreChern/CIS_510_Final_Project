3d0c0c72115026468108b5ae7d2d145a1e931cbe
Alexandre Chen <napoleonalexandre@gmail.com>
2020-06-07 14:27:53 -0700

    modified heat1d_2.jl

--- heat1d_2.jl
+++ heat1d_2.jl
@@ -23,7 +23,7 @@ function surf_bc(t, Δt)
     ω_d = Δt/(24)
     T_a = 30
     ω_a = Δt/(24*365)
-    return 288 + (T_d * sin.(2*pi*ω_d*t))# - T_a * sin.(ω_a*t))
+    return 288 + (T_d * sin.(2*pi*ω_d*t)) # - T_a * sin.(ω_a*t))
 end
 
 #=
@@ -49,7 +49,7 @@ function exact(t,Δt,z,α)
     return init_cond(z)+(exp.((-1).*z.*sqrt(2*pi*ω_d/(2α))).*T_d.*sin.(2*pi*ω_d.*t.-z.*sqrt(2*pi*ω_d/(2α))))#+exp.(-z.*sqrt(ω_a/(2α))).*sin.(ω_a.*t.-z.*sqrt(ω_a/(2α))))
 end
 
-function time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, initial, exact, odesolve,num_th_block=0, num_block=0)
+function time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, initial, exact, bound_cond, odesolve,num_th_block=0, num_block=0)
     N  = Integer(ceil((10-0)/Δz)) # N+1 total nodes, N-1 interior nodes
     z = 0:Δz:10
     t = 0:Δt:tf
@@ -63,19 +63,15 @@ function time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, initial, exact, odesol
     # A[N+1,N+1]=(α/Δz^2)*1
     # A[2,1]=(α/Δz^2)*1
     # A[N,N+1]=(α/Δz^2)*1
-
-    A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N+1,1:N,ones(N),N+1,N+1) +
-         sparse(1:N,2:N+1,ones(N),N+1,N+1))
-    
+    #println(A)
+    A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N+1,1:N,ones(N),N+1,N+1) + sparse(1:N,2:N+1,ones(N),N+1,N+1))
 
     u = Array{Float64}(zeros(N+1)) # Interior Nodes
     u .= exact(0,Δt,z[1:N+1],α)
     #println(u)
 
     #(t, U_inter, E_inter) = odesolve(Δt, t1, tf, u, A, my_exact)
-    #(t, U, E) = odesolve(z, Δt, t1, tf, u, A, α, β, exact, surf_bc)
-     (U, E, t) = my_forward_Euler(z, Δt, t1, tf, A, u, N, M, exact, surf_bc, α, β)
-
+    (t, U, E) = odesolve(z, Δt, t1, tf, u, A, α, β, exact, bound_cond)
 #=
     init = Array{Float64}(zeros(M+1,2))
     init[:,1] .= α
@@ -88,61 +84,180 @@ function time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, initial, exact, odesol
 
 end
 
-k = 2.7
-Cp = 790
-ρ = 2700
-Δz = 1
-λ = 0.1
-# D =
-Δt = round(λ*Δz^2, digits = 12)
-#@assert 1 >= 2 * (k*Δt)/(Δx^2)
-tf = 480
-t1 = 0
-α = k/(ρ*Cp) #Thermal Diffusivity
-#α =
-#β =
-β = init_cond(10) #boundary condition
-N  = Integer(ceil((10-0)/Δz))
+# function time_dependent_heat_GPU(k, Δz, Δt, tf ,t1, α, β, initial, exact, odesolve,num_th_block=0, num_block=0)
+#     N  = Integer(ceil((10-0)/Δz)) # N+1 total nodes, N-1 interior nodes
+#     z = 0:Δz:10
+#     t = 0:Δt:tf
+#     M = Integer(ceil((tf-0)/Δt)) # M+1 total temporal nodes
+#
+#     # A is N-1 by N-1 because it is the interior nodes
+#
+#     # A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N,1:N-1,ones(N-1),N+1,N+1) +
+#     #     sparse(2:N,3:N+1,ones(N-1),N+1,N+1))
+#     # A[1,1]=(α/Δz^2)*1
+#     # A[N+1,N+1]=(α/Δz^2)*1
+#     # A[2,1]=(α/Δz^2)*1
+#     # A[N,N+1]=(α/Δz^2)*1
+#     #println(A)
+#     A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N+1,1:N,ones(N),N+1,N+1) + sparse(1:N,2:N+1,ones(N),N+1,N+1))
+#
+#     u = Array{Float64}(zeros(N+1)) # Interior Nodes
+#     u .= exact(0,Δt,z[1:N+1],α)
+#     #println(u)
+#
+#     #(t, U_inter, E_inter) = odesolve(Δt, t1, tf, u, A, my_exact)
+#     (all_t, U, E) = odesolve(z, Δt, t1, tf, u, A, α, β, exact, initial, num_th_block, num_block)
+# #=
+#     init = Array{Float64}(zeros(M+1,2))
+#     init[:,1] .= α
+#     init[:,2] .= β
+#
+#     U .= U_inter#vcat(init[:,1]', U_inter, init[:,2]')
+#     E .= E_inter#vcat(init[:,1]', E_inter, init[:,2]')
+# =#
+#     return (z, t, U, E)
+#
+# end
 
-num_th_blk = 32
-num_block = cld(N, num_th_blk)
 
-#(x, t, U, E) = time_dependent_heat(k, Δx, Δt, tf ,t1, α, β, f, exact, naive_rk4)
-(z, t, U, E) = time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, init_cond, exact, naive_rk4, num_th_blk, num_block)
+Δz_list = [2, 1, 0.5, 0.25, 0.125]
+Δz_listt = [2,1,0.5,0.25]
 
-#=
-xfine = 0:Δx/20:1
+for Δz in Δz_listt
+    k = 2.7
+    Cp = 790
+    ρ = 2700
+    # Δz = 1
+    @show Δz
+    λ = 0.1
+    Δt = round(λ*Δz^2, digits = 12)
+    @show Δt
+    #@assert 1 >= 2 * (k*Δt)/(Δx^2)
+    tf = 480
+    t1 = 0
+    α = k/(ρ*Cp) #Thermal Diffusivity
+    #α =
+    #β =
+    β = init_cond(10) #boundary condition
+    N  = Integer(ceil((10-0)/Δz))
 
-stride_time = 100
+    num_th_blk = 32
+    num_block = cld(N, num_th_blk)
 
-for i = 1:1:10
+    #(x, t, U, E) = time_dependent_heat(k, Δx, Δt, tf ,t1, α, β, f, exact, naive_rk4)
+    (z, t, U, E) = time_dependent_heat(k, Δz, Δt, tf ,t1, α, β, init_cond, exact, surf_bc, naive_rk4, num_th_blk, num_block)
 
-    p = plot(x,U[:,i],label = "approx", shape = :circle, color = :blue)
-    ylims!((0,1))
-    display(p)
+    #=
+    xfine = 0:Δx/20:1
 
-    Efine = exact(xfine, t[i])
-    pexact = plot!(xfine,Efine, label = "exact", color = :red)
-    display(pexact)
+    stride_time = 100
 
-    sleep(1)
-end
+    for i = 1:1:10
 
-=#
-finegrd = 0:0.001:10
-finex = exact(tf, Δt, finegrd, α)
-err = sqrt(Δz) * norm(U[:,end]- E[:,end])
+        p = plot(x,U[:,i],label = "approx", shape = :circle, color = :blue)
+        ylims!((0,1))
+        display(p)
 
-E - U
+        Efine = exact(xfine, t[i])
+        pexact = plot!(xfine,Efine, label = "exact", color = :red)
+        display(pexact)
 
-println(err)
-for i = size(U,2)
+        sleep(1)
+    end
+
+    =#
+
+    @show err = sqrt(Δz) * norm(U[:,end]- E[:,end])
+    @show (U[10:end,end] - E[10:end,end])
+    @show rel_err = norm(U[:,end] - E[:,end])/norm(E[:,end])
+    println(err)
+    for i = size(U,2)
     p = plot(U[1:end,i],z[1:end],#=label = string("Numerical",string(Δt)),=# yflip = true, shape = :circle, color = :blue)
-    display(p)
     #ylims!((0.0,1.0))
     xlabel!("Temperature (K)")
     ylabel!("Depth (m)")
-    pexact = plot!(finex,finegrd, #= label = string("Exact",string(Δt)),=# yflip = true, color = :red)
-    display(pexact)
+    pexact = plot!(E[1:end,i],z[1:end], #= label = string("Exact",string(Δt)),=# yflip = true, color = :red)
     savefig(string("CPUplot",string(Δz),".png"))
+end
+
+end
+
+# k = 2.7
+# Cp = 790
+# ρ = 2700
+# Δz = 0.5
+# @show Δz
+# λ = 0.1
+# Δt = round(λ*Δz^2, digits = 12)
+# @show Δt
+# #@assert 1 >= 2 * (k*Δt)/(Δx^2)
+# tf = 480
+# t1 = 0
+# α = k/(ρ*Cp) #Thermal Diffusivity
+# #α =
+# #β =
+# β = init_cond(10) #boundary condition
+# N  = Integer(ceil((10-0)/Δz))
+#
+# num_th_blk = 32
+# num_block = cld(N, num_th_blk)
+#
+# #(x, t, U, E) = time_dependent_heat(k, Δx, Δt, tf ,t1, α, β, f, exact, naive_rk4)
+# time_dependent_heat_GPU(k, Δz, Δt, t1 ,tf, α, β, init_cond, exact, cu_naive_rk4, num_th_blk, num_block)
+
+
+
+function solve_GPU(k,Δz,Δt,t1,tf,α,β,init_cond,exact,num_th_block,num_block)
+    N  = Integer(ceil((10-0)/Δz)) # N+1 total nodes, N-1 interior nodes
+    z = 0:Δz:10
+    t = 0:Δt:tf
+    M = Integer(ceil((tf-0)/Δt)) # M+1 total temporal nodes
+
+    # A is N-1 by N-1 because it is the interior nodes
+
+    # A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N,1:N-1,ones(N-1),N+1,N+1) +
+    #     sparse(2:N,3:N+1,ones(N-1),N+1,N+1))
+    # A[1,1]=(α/Δz^2)*1
+    # A[N+1,N+1]=(α/Δz^2)*1
+    # A[2,1]=(α/Δz^2)*1
+    # A[N,N+1]=(α/Δz^2)*1
+    #println(A)
+    A = (α/Δz^2)*(-2 * sparse(1:N+1,1:N+1,ones(N+1),N+1,N+1) + sparse(2:N+1,1:N,ones(N),N+1,N+1) + sparse(1:N,2:N+1,ones(N),N+1,N+1))
+
+    u = Array{Float64}(zeros(N+1)) # Interior Nodes
+    u .= exact(0,Δt,z[1:N+1],α)
+
+    #println(u)
+
+    #(t, U_inter, E_inter) = odesolve(Δt, t1, tf, u, A, my_exact)
+    num_th_block = 32
+    num_block = cld(N, num_th_blk)
+    (all_t, U, E) = cu_naive_rk4(z,Δt, t1, tf, u, A, α, β, exact, init_cond, num_th_block, num_block)
+    return (all_t, U, E)
+end
+
+
+let
+    k = 2.7
+    Cp = 790
+    ρ = 2700
+    Δz = 1
+    @show Δz
+    λ = 0.1
+    Δt = round(λ*Δz^2, digits = 12)
+    @show Δt
+    #@assert 1 >= 2 * (k*Δt)/(Δx^2)
+    tf = 480
+    t1 = 0
+    α = k/(ρ*Cp) #Thermal Diffusivity
+    #α =
+    #β =
+    β = init_cond(10) #boundary condition
+    N  = Integer(ceil((10-0)/Δz))
+
+    num_th_blk = 32
+    num_block = cld(N, num_th_blk)
+
+
+    (all_t, U, E) = solve_GPU(k,Δz,Δt,t1,tf,α,β,init_cond,exact, num_th_blk, num_block)
 end
